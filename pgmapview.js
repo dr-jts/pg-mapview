0d214a8a990d668cfb6d4684a196b43926c56a65
Martin Davis <mtnclimb@gmail.com>
2020-01-12 13:42:53 -0800

    Use object function short notation

--- pgmapview.js
+++ pgmapview.js
@@ -18,19 +18,19 @@ function addLayerDataset(name, url) {
     });
     return lyr;
 }
-class OAF {
-    static urlItems(host, name) {
+OAF = {
+    urlItems(host, name) {
         var url = `${host}/collections/${name}/items`;
         return url;
-    }
-    static urlWithParams(url, param) {
+    },
+    urlWithParams(url, param) {
         url = OAF.addQueryParam(url, "limit", param.limit);
         url = OAF.addQueryParam(url, "bbox", param.bbox);
         url = OAF.addQueryParam(url, "precision", param.precision);
         url = OAF.addQueryParam(url, "transform", param.transform);
         return url;
-    }
-    static addQueryParam(url, name, value) {
+    },
+    addQueryParam(url, name, value) {
         if (! value || value.length <= 0) return url;
         let hasQuery = url.indexOf('?') >= 0;
         let  delim = hasQuery ? '&' : '?';
